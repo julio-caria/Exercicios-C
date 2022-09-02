51602730a11ec588469e5816de205c6ca20ff7d0
Júlio Caria <juliocvc2004@hotmail.com>
2022-09-02 14:38:08 -0300

    Exercícios de Estrutura de Decisão

---
+++ 001_EstruturaCondicional.c
@@ -0,0 +1,22 @@
+// 1.Faça um Programa que peça dois números e imprima o maior deles.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    int number1,
+        number2;
+
+    printf("Verificando qual numero digitado e maior");
+    printf("");
+    scanf("");
+
+    printf("");
+    printf("");
+
+    printf("\n\n");
+    system("pause");
+}
